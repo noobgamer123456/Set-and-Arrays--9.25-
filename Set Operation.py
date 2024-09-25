e5dfcde4e86e9e19e72a347dcc1d5d3aae3a932e
Tabid Hossain <tabid.hossain2009@gmail.com>
2024-09-25 21:26:30 +0600

    first commit

---
+++ Set Operation.py
@@ -0,0 +1,11 @@
+mySet = {2,1,3, "Zabir","Zabir","Zabir","Tabid"}
+print(mySet)
+
+mySet.add("Nahian Sir")
+print(mySet)
+mySet.pop()
+print(mySet)
+
+lst = [555,666,777]
+setList = set(lst)
+print(setList,type(setList))
